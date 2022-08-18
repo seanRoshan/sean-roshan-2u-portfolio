741a9b202202ee9bc0f967cc0f2150c3d76bd2f3
Sean Roshan <sean.roshan.91@gmail.com>
2022-08-18 00:47:37 -0400

    application about me contains an image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,23 @@
-import { Card } from 'semantic-ui-react'
+import { Card, Image, Segment, Container } from 'semantic-ui-react'
+import profile_photo from "../../assets/profile_photo.jpg";
 
-function About(){
-    return(
-        <Card 
-        raised
-        fluid
-        header='About Me'
-        color='yellow' 
-        description="I'm a self-motivated and visionary software developer with over seven years of experience producing robust and scalable applications. I can analyze requirements and provide creative solutions to complex problems with my deep knowledge of system design and top-notch coding skills. In my previous role, I designed an application to automate the dentist's credentialing process for insurance companies, reducing processing time by 90%. Furthermore, on a team of five developers, I helped migrate a monolithic legacy Apache Wicket application to a modern Spring-Angular framework with micro-services architecture, opening new revenue opportunities for my employer. My most recent project was to create a web application from scratch for a venture capital company, potentially generating multi-million dollars annually.">
+function About() {
+    return (
+        <Card
+            raised
+            fluid
+            color='yellow'>
+            <Card.Header><h1>About Me</h1></Card.Header>
+            <Card.Description>
+                <div className='aboutme-description'>
+                    <Image size='small' src={profile_photo} floated='left' />
+                    <Container textAlign='justified'>
+                        <p >
+                            I'm a self-motivated and visionary software developer with over seven years of experience producing robust and scalable applications. I can analyze requirements and provide creative solutions to complex problems with my deep knowledge of system design and top-notch coding skills. In my previous role, I designed an application to automate the dentist's credentialing process for insurance companies, reducing processing time by 90%. Furthermore, on a team of five developers, I helped migrate a monolithic legacy Apache Wicket application to a modern Spring-Angular framework with micro-services architecture, opening new revenue opportunities for my employer. My most recent project was to create a web application from scratch for a venture capital company, potentially generating multi-million dollars annually.
+                        </p>
+                    </Container>
+                </div>
+            </Card.Description>
         </Card>
     )
 }
